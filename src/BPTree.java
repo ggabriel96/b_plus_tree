2349592ba69c69c54652d3b1566cd35ef7f37fdb
Gabriel Galli <g7.galli96@gmail.com>
2015-05-19 11:39:39 -0300

    Started coding find() and add() methods

--- src/BPTree.java
+++ src/BPTree.java
@@ -10,8 +10,34 @@ class BPTree {
 	public BPTree(int key) throws IOException {
 		this.root = new BPNode(count, key);
 		this.dw = new DiskWriter("bp.tree");
-		dw.write(this.root);
-		System.out.println(dw.read(0));
+		this.dw.write(this.root);
+		System.out.println(this.dw.read(0));
 		count++;
+	}
+	
+	public BPNode find(int key) throws IOException {
+		int i, j;
+		Key child = null ;
+		BPNode node = null;
+		
+		for (i = 0; i * 4 < this.dw.fileLength(); i++) {
+			node = this.dw.read(i);
+			
+			for (j = 0; j < BPTree.MAX; j++) {
+				child = node.getChild(j);
+				
+				if (child != null) {
+					// insert method of the TreeMap! \o/
+					// just take care of the size of the node
+				}
+			}
+		}
+		
+		return node;
+	}
+	
+	public boolean add(int key) throws IOException {
+		BPNode node = this.find(key);
+		return false;
 	}
 }
