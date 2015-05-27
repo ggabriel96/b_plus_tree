839b019dce3faf64bb23fd4105026d3a41748ad2
Gabriel Galli <g7.galli96@gmail.com>
2015-05-27 17:45:12 -0300

    Gotta analyze this tree...

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws IOException {
 		BPTree t = new BPTree(1);
+		t.add(11);
+		t.add(10);
+		t.add(9);
+		t.add(8);
+		t.add(7);
 		t.add(2);
+		t.add(3);
 		t.add(4);
 		t.add(5);
-		System.out.println(t.root);
-		t.add(3);
 		t.add(6);
-		t.add(7);
 		System.out.println(t.root);
 		System.out.println("************************************");
 		t.root.print();
